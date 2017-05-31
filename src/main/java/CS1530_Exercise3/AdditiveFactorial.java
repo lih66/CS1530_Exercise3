3d400705c843c7d109e182c370d847079548d6a2
Ling Han <lih66@pitt.edu>
2017-05-31 15:30:57 -0400

    final

--- src/main/java/CS1530_Exercise3/AdditiveFactorial.java
+++ src/main/java/CS1530_Exercise3/AdditiveFactorial.java
@@ -23,7 +23,7 @@ public class AdditiveFactorial {
     }
 	public static void main(String[] args) 
 	{
- 		if (args.length == 0) 
+ 		if(args.length == 0) 
  		{
  			System.out.println("Enter something buddy");
  			System.exit(1);
