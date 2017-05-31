572777c9798ae4914c77e5dbf93adef9b626a086
Ling Han <lih66@pitt.edu>
2017-05-31 15:14:05 -0400

    final

---
+++ FactorialTest.java
@@ -0,0 +1,36 @@
+package CS1530_Exercise3;
+import org.junit.Test;
+import static org.junit.Assert.*;
+// import CS1530_Exercise3.AdditiveFactorial;
+
+public class FactorialTest {
+	fdsfds
+ @Test
+ public void testFactorialTrig1() {
+ 	assertEquals(AdditiveFactorial.Trig(3), 16);
+ }
+
+ @Test
+ public void testFactorialTrig2() {
+ 	assertEquals(AdditiveFactorial.Trig(5), 15);
+ }
+ @Test
+ public void testFactorialTrig3() {
+ 	assertEquals(AdditiveFactorial.Trig(1), 1);
+ }
+
+ @Test
+ public void testFactorialLazy1() {
+ 	assertEquals(AdditiveFactorial.Lazy(5), 16);
+ }
+
+ @Test
+ public void testFactorialLazy2() {
+ 	assertEquals(AdditiveFactorial.Lazy(9), 46);
+ }
+
+ @Test
+ public void testFactorialLazy3() {
+ 	assertEquals(AdditiveFactorial.Lazy(3), 7);
+ }
+}
---
+++ src/main/java/CS1530_Exercise3/AdditiveFactorial.java
@@ -0,0 +1,73 @@
+package CS1530_Exercise3;
+public class AdditiveFactorial {
+	public static int Trig(int num) 
+	{
+		if (num <= 1) {
+			return 1;
+		} 
+		else 
+		{
+			return num + Trig(num - 1);
+		}
+	}
+    public static long Lazy(long num) 
+    {
+    	if (num < 1) 
+    	{
+    		return 1;
+    	} 
+    	else 
+    	{
+    		return num + Lazy(num - 1);
+    	}
+    }
+	public static void main(String[] args) 
+	{
+ 		if (args.length == 0) 
+ 		{
+ 			System.out.println("Enter something buddy");
+ 			System.exit(1);
+		 } 
+	 	else 
+ 		{		 
+ 			String name = args[0];
+ 			String one = "triangle";
+ 			String two = "lazy";
+ 			
+ 			if(name.equals(one) || name.equals(two))
+ 			{
+ 				//OKAY
+ 			}
+			else
+			{
+ 				System.out.println("First arugment is incorrect!");
+ 				System.exit(1);
+ 			}
+ 		
+ 			//***********
+ 			if(args[1] != null)
+ 			{
+ 				if(name.equals(one))
+ 	 			{
+ 					try {
+ 	 					 int x = Trig(Integer.parseInt(args[1]));
+ 	 					 System.out.println("Tri(" + args[1] + ") = " + x);
+ 	 					 } catch (NumberFormatException ex) {
+ 	 					 System.out.println("Second arugment is not an integer!");
+ 	 					 System.exit(2); 
+ 	 					 }
+ 	 			}
+ 	 			else
+ 	 			{
+ 	 				try {
+	 					long y = Lazy(Long.parseLong(args[1]));
+	 					System.out.println("Lazy(" + args[1] + ") = " + y);
+	 			  	  	} catch (NumberFormatException ex) {
+	 					System.out.println("Enter an integer!");
+	 					System.exit(1);
+	 			  	  }
+ 	 			}	
+ 	 		}
+ 	 	}
+ 	}
+}
